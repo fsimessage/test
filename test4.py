e1f3c3b421d7623cda51adceeaabfcbde360683c
direct1986 <07ky@sina.com>
2023-06-14 23:17:22 +0800

    Update test5.py

--- test4.py
+++ test4.py
@@ -10,10 +10,10 @@ options.add_argument("--no-sandbox")
 s = Service('bin/chromedriver')
 driver = webdriver.Chrome(service=s, options=options)
 
-res = driver.get('https://www.baidu.com')
+driver.get('https://www.baidu.com')
 # 睡眠3秒
 time.sleep(3)
-print(res)
+print(driver.title)
 # 关闭浏览器
 driver.quit()
 cookie_use_times = time.time()
