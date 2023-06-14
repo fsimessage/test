084d36a9cae61ada7e23eca87391309bdb01d4bc
direct1986 <07ky@sina.com>
2023-06-14 22:28:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,85 +1,24 @@
-import requests, random, time
-from datetime import datetime
-from datetime import timedelta
-from bs4 import BeautifulSoup
-import json
+# -*- coding: utf-8 -*-
+import time
+from selenium import webdriver
 
+driver_path = "/data/selenium/chromedriver"
 
-# todo 获取access_token
-def get_token():
-    respon = requests.get(
-        f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={"wxf63dab6abc2027ac"}&secret={"11c7d8db9840bf6f6926cc69b28dd86e"}')
-    content = respon.content
-    content = content.decode('utf-8')
-    data = json.loads(content)
-    token = data.get("access_token")
-    if token:
-        return token
+# 创建chrome参数对象
+opt = webdriver.ChromeOptions()
+# 把chrome设置成为无界面模式
+opt.add_argument('--headless')
+# 创建chrome无界面对象
+driver = webdriver.Chrome(options=opt, executable_path=driver_path)
+driver.implicitly_wait(100)
+time.sleep(2)
 
-
-def uniformMessage_send(weapp_template_msg):
-    """统一服务消息"""
-    token = get_token()
-    if not token:
-        return False
-    url = "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=" + token
-    if weapp_template_msg:
-        response = requests.post(url, json=weapp_template_msg)
-        content = response.content.decode('utf-8')
-        data = json.loads(content)
-        print(data)
-
-def write_json(data):
-    f = open('alllaw1.txt','w',encoding='utf-8')
-    f.write(data)
-    f.close()
-
-def UserAgent():
-    user_agent_list = [
-        'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36',
-        'Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36',
-        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0.6',
-        'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36',
-        'Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36']
-    UserAgent = {'User-Agent': random.choice(user_agent_list)}
-    return UserAgent
-
-
-def sendwxmessage(message):
-    ####企业微信消息应用ID
-    AgentId = '1000003'
-    Secret = 'ysZKeQh_Czx8QO5bFpex8A-zJBm_JLjW0yD4p_d9SlQ'
-    CompanyId = 'ww53c0e8c78ee4b0de'
-
-    print(message)
-    # /////////////////
-
-    r = requests.post(
-        f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={CompanyId}&corpsecret={Secret}').json()
-    ACCESS_TOKEN = r["access_token"]
-    data = {
-        "touser": "@all",
-        "msgtype": "text",
-        "agentid": f"{AgentId}",
-        "text": {"content": f"{message}"}
-    }
-    data = json.dumps(data)
-    r = requests.post(f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={ACCESS_TOKEN}',
-                      data=data)
-
-def main():
-    try:
-
-        sendwxmessage("FSI消息测试")
-        print('发送消息')
-            
-    except Exception as e:
-        print(e)
-
-if __name__ == '__main__':
-    main()
+# 打开浏览器，模拟浏览器请求页面
+res = driver.get('https://www.baidu.com')
+# 睡眠3秒
+time.sleep(3)
+print(res)
+# 关闭浏览器
+driver.quit()
+cookie_use_times = time.time()
+print("quit")
